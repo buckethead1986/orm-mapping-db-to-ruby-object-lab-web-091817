b7fa507a51c29da0aa2e0de76e5262e2746fcf09
buckethead1986 <rspeckpfs@gmail.com>
2017-09-25 22:28:28 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10 LIMIT 1
+    SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT 1
     SQL
     student = DB[:conn].execute(sql)
     self.new_from_db(student[0])
